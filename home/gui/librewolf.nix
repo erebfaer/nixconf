43562bd6e940a4f7a09f322745149c7ae4dd27cb
nk <erebfaer@hotmail.com>
2023-11-30 22:05:10 +1100

    testing arkenfox

--- home/gui/librewolf.nix
+++ home/gui/librewolf.nix
@@ -1,27 +1,12 @@
 { inputs, outputs, config, lib, pkgs, ...}:
 
 {
-  imports = [
-    inputs.arkenfox.hmModules.arkenfox
-  ];
-  
-  programs.firefox = {
+   
+  programs.librewolf = {
     enable = true;
     package = (pkgs.firefox.override { nativeMessagingHosts = [
         inputs.pipewire-screenaudio.packages.${pkgs.system}.default ]; });
     
-
-    #nativeMessagingHosts = [
-    #  inputs.pipewire-screenaudio.packages.${pkgs.system}.default
-    #];
-    
-    arkenfox = {
-      enable = true;
-      version = "119.0"; 
-    };
-
-
-
     profiles = {
       default = {
         extensions = with pkgs.nur.repos.rycee.firefox-addons; [
@@ -40,10 +25,6 @@
           sponsorblock
           dearrow
         ];
-
-        arkenfox = {
-          enable = true;
-        };
       };
     };
   };
