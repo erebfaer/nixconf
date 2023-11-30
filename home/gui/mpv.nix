8a33822097b78159ffa3bbb3c66fecfccb3513f6
nk <erebfaer@hotmail.com>
2023-11-30 18:03:53 +1100

    mpv config

--- home/gui/mpv.nix
+++ home/gui/mpv.nix
@@ -3,10 +3,110 @@
   programs.mpv = {
     enable = true;
 
+    bindings = {
+      MBTN_MID = "script-binding uosc/menu";
+      MBTN_RIGHT = "script-binding playlistmanager/showplaylist";
+      MBTN_LEFT = "cycle pause";
+      MBTN_LEFT_DOUBLE = "cycle fullscreen";
+      "x" = "set sub-delay 0";
+    };
+
+    config = {
+      #general
+      save-position-on-quit = "yes";
+      keep-open = "yes";
+      border = "no";
+      osc = "no";
+      osd-bar = "no";
+
+      # cache
+      cache = "yes";
+      # cache-secs = 300 # bigger cache
+      demuxer-max-back-bytes = "20M";
+      demuxer-max-bytes = "20M";
+
+      # audio
+      # ao = ???
+      audio-channels = "stereo";
+      audio-normalize-downmix = "yes";
+      alang="jpn,jp,jap,Japanese,eng,en,enUS,en-US,English";
+      audio-pitch-correction = "yes";
+
+      # subtitles
+      blend-subtitles = "no";
+      slang = "forced,eng,en,enUS,en-US,English";
+
+      # video
+      profile = "gpu-hq";
+      vo = "gpu-next";
+      video-output-levels = "limited";
+      deinterlace = "no";
+      dither-depth = "auto";
+      hwdex = "auto-safe";
+      
+      ## debanding
+      deband = "yes";
+      deband-iterations = 4;
+      deband-threshold = 35;
+      deband-range = 16;
+      deband-grain = 4;
+
+      ## antiring
+      scale-antiring = 0.7;
+      dscale-antiring = 0.7;
+      cscale-antiring = 0.7;
+
+      ## interpolation
+      interpolation = "yes";
+      tscale = "sphinx";
+      tscale-blur = 0.6991556596428412;
+      tscale-radius = 1.05;
+      tscale-clamp = 0.0;
+
+      ## SDR/HDR
+      tone-mapping = "bt.2446a";
+
+      ## Shaders
+      # TODO
+      ### Luma up
+      ### Luma down
+      ### Chroma up/down
+      
+    };
+    
     scripts = with pkgs.mpvScripts; [
       mpv-playlistmanager-unstable
       mpv-thumbfast
       uosc
-    ]; 
+    ];
+
+    scriptOpts = {
+      
+      playlistmanager = {
+        
+        dynamic_binds = "yes";
+        key_moveup = "UP WHEEL_UP";
+        key_movedown = "DOWN WHEEL_DOWN";
+        key_playfile = "ENTER MBTN_LEFT";
+        ket_removefile = "BS MBTN_MID";
+        key_closeplaylist = "ESC MBTN_RIGHT";
+
+        playlist_display_timeout = 5;
+        # showamount=28
+
+        # TODO add %pos to the playlist file templates (sym %pos. %name)
+      };
+
+      uosc = {
+        timeline_size = 20;
+        timeline_persistency = "paused";
+        progress = "never";
+        click_threshold = 200;
+        proximity_in = 10;
+        proximity_out = 80;
+        destination_time = "total";
+        autohide = "yes";
+      };
+    };
   };
 }
