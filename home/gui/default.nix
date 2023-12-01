cee8364f66ed5e6ec4086e9635a5a4016a426097
nk <erebfaer@hotmail.com>
2023-12-01 22:31:06 +1100

    obs

--- home/gui/default.nix
+++ home/gui/default.nix
@@ -21,5 +21,13 @@
     home-manager.enable = true;
     git.enable = true;
     wezterm.enable = true;
+    obs-studio = {
+      enable = true;
+      plugins = with pkgs.obs-studio-plugins; [
+        wlrobs
+        obs-backgroundremoval
+        obs-pipewire-audio-capture
+      ];
+    };
   };
 }
