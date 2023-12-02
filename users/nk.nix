2c37f4688f0e031fec71ae72eb0bf1cf14e5bc9c
nk <erebfaer@hotmail.com>
2023-12-02 19:14:01 +1100

    gamescope?

--- users/nk.nix
+++ users/nk.nix
@@ -17,5 +17,32 @@
     ];
   };
 
-  programs.steam.enable = true;
+  programs = {
+    steam = {
+      enable = true;
+      package = pkgs.steam.override {
+        extraEnv = {};
+        extraLibraries = pkgs: with pkgs; [
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXinerama
+          xorg.libXScrnSaver
+          libpng
+          libpulseaudio
+          libvorbis
+          stdenv.cc.cc.lib
+          libkrb5
+          keyutils
+        ];
+      };
+      gamescopeSession = {
+        enable = true;
+        args = [];
+      };
+    };
+    gamescope = {
+      enable = true;
+      args = [];
+    };
+  };
 }
